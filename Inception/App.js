7eadf9fa17e6723240c41e2b821bfee26d198634
Hemant Agrawal <hemant.agrawal@bold.com>
2025-02-19 21:35:55 +0530

    React in js not jsx
    we can write React in JS just fine like in App.js, but the code might not look very readable, that's why React is generally written in JSX

--- Inception/App.js
+++ Inception/App.js
@@ -1,9 +1,20 @@
-const heading = React.createElement(
-  'h1',
-  { id: 'heading' },
-  'Hello world from React'
-);
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child1' }, [
+    React.createElement('h1', { id: 'heading' }, "I'm an h1 inside child1"),
+    React.createElement('h2', {}, "I'm an h2 inside child1"),
+  ]),
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement('h1', {}, "I'm an h1 inside child2"),
+    React.createElement('h2', {}, "I'm an h2 inside child2"),
+  ]),
+]);
+
+// const heading = React.createElement(
+//   'h1',
+//   { id: 'heading' },
+//   'Hello world from React'
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(parent);
