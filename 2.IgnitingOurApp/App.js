a142641357e66bbd24a3c6f063049156cb9aa984
Hemant Agrawal <hemantagrawal.gvr@gmail.com>
2025-02-21 00:19:16 +0530

    initialize npm and install parcel bundler
    understand the need of npm and bundler

---
+++ 2.IgnitingOurApp/App.js
@@ -0,0 +1,26 @@
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child1' }, [
+    React.createElement('h1', { id: 'heading' }, "I'm an h1 inside child1"),
+    React.createElement('h2', {}, "I'm an h2 inside child1"),
+  ]),
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement('h1', {}, "I'm an h1 inside child2"),
+    React.createElement('h2', {}, "I'm an h2 inside child2"),
+  ]),
+]);
+
+console.log(parent);
+
+// const heading = React.createElement(
+//   'h1',
+//   { id: 'heading' },
+//   'Hello world from React'
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+
+console.log(root);
+
+root.render(parent);
+root2.render(parent);
