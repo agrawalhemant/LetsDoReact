a8449bad5cb2202d2091c514dfe2d6f47b99a754
Hemant Agrawal <hemantagrawal.gvr@gmail.com>
2025-02-21 19:24:42 +0530

    What is JSX?
    Understand what is JSX and how does it makes developers' life easy

---
+++ 3.LayingTheFoundation/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// const heading = React.createElement('h1', null, 'heading text');
+const heading = (
+  <h1 id="heading" className="heading" tabIndex="1">
+    heading using JSX
+  </h1>
+);
+const link = (
+  <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+    Click here to visit Google
+  </a>
+);
+const image = (
+  <img
+    src="https://cdn-icons-png.flaticon.com/512/8136/8136031.png"
+    alt="Placeholder Image"
+  />
+);
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render([heading, link, image]);
