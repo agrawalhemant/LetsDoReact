d6c08f00dc77cfeb53ac82c9322eaf159b68d999
Hemant Agrawal <hemantagrawal.gvr@gmail.com>
2025-03-05 00:00:21 +0530

    make state variables in class component

--- 8.LetsGetClassy/src/Components/User.js
+++ 8.LetsGetClassy/src/Components/User.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import { useState } from 'react';
+
 const User = ({ name, location }) => {
+  const [count1, setCount1] = useState(1);
+  const [count2, setCount2] = useState(2);
   return (
     <div id="user-card">
+      <h1>Count1: {count1}</h1>
+      <h1>Count2: {count2}</h1>
       <h2>Name: {name}</h2>
       <h3>Location: {location}</h3>
       <h3>Contact: @agrawalhemant</h3>
--- 8.LetsGetClassy/src/Components/UserClass.js
+++ 8.LetsGetClassy/src/Components/UserClass.js
@@ -3,11 +3,18 @@ import React from 'react';
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count1: 0,
+      count2: 2,
+    };
   }
   render() {
-    const { name, location, contact } = this.props;
+    const { name, location } = this.props;
+    const { count1, count2 } = this.state;
     return (
       <div id="user-card">
+        <h1>Count1: {count1}</h1>
+        <h1>Count2: {count2}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h3>Contact: @agrawalhemant</h3>
